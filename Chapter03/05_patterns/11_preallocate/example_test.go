6b861353326415334f8d10817a8620c40051f690
Corey Scott <corsc@users.noreply.github.com>
2022-05-23 21:31:02 +1000

    Updated based on edit/draft of Ch03

--- Chapter03/05_patterns/11_preallocate/example_test.go
+++ Chapter03/05_patterns/11_preallocate/example_test.go
@@ -8,14 +8,16 @@ import (
 // goos: darwin
 // goarch: amd64
 // pkg: github.com/corsc/Beyond-Effective-Go/Chapter03/05_patterns/11_preallocate
-// BenchmarkExample-8   	     100	 120309432 ns/op
-// BenchmarkFixed-8     	     200	  89336293 ns/op
+// cpu: Intel(R) Core(TM) i7-4870HQ CPU @ 2.50GHz
+// BenchmarkExample-8   	     145	  85199634 ns/op
+// BenchmarkFixed-8     	    1029	  11776375 ns/op
 
 func BenchmarkExample(b *testing.B) {
 	total := 1000000
-	var data []string
 
 	for i := 0; i < b.N; i++ {
+		var data []string
+
 		for x := 0; x < total; x++ {
 			data = append(data, "x")
 		}
@@ -24,11 +26,12 @@ func BenchmarkExample(b *testing.B) {
 
 func BenchmarkFixed(b *testing.B) {
 	total := 1000000
-	data := make([]string, total)
 
 	for i := 0; i < b.N; i++ {
+		data := make([]string, total)
+
 		for x := 0; x < total; x++ {
-			data = append(data, "x")
+			data[x] = "x"
 		}
 	}
 }
